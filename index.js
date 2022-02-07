59ebe0a6b40ca24978e48de16c06f060f1b84aaf
Stiizzy Cat <90114741+StiizzyCat@users.noreply.github.com>
2022-02-07 10:20:39 -0800

    Updated with Functions (i was bored)

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ var counter = require('counter');
 const chalk = require('chalk');
 fs = require('fs');
 count = counter(0);
+const staticPool = new StaticPool({
+	size: 10,
+	task: (n) => n + 1
+});
 console.log(' ███▄ ▄███▓ ▄▄▄       ██▓███   ▄▄▄▄    ▒█████   ███▄ ▄███▓ ▄▄▄▄   ▓█████  ██▀███  ')
 console.log(' ▓██▒▀█▀ ██▒▒████▄    ▓██░  ██▒▓█████▄ ▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▓█   ▀ ▓██ ▒ ██▒')
 console.log(' ▓██    ▓██░▒██  ▀█▄  ▓██░ ██▓▒▒██▒ ▄██▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒███   ▓██ ░▄█ ▒')
@@ -25,25 +29,26 @@ console.log('                                          ░                      
 var poop = os.cpus()[0].model
 console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤv1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
 console.log(chalk.bgMagenta(`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤYour Cpu Specs: ${poop}ㅤㅤㅤㅤㅤㅤㅤㅤㅤ`))
+
 var website = prompt('Enter The website url: ');
-const staticPool = new StaticPool({
-	size: 10,
-	task: (n) => n + 1
-});
-console.log(chalk.greenBright('Initalizing Script!'))
-var wait = setTimeout(() => {
-/* the Threads Start
- im new to this threads package so if im doing it wrong please tell me :smile: */
-staticPool.exec(10).then((result) => {
-	var t = 0
-	var int = setInterval(() => {
-		axios.get(website).catch(error => {
-            console.log(chalk.redBright('ERROR OCCURED, SCRIPT STOPPING!!!')) 
-	    process.exit() 
-        })
-		console.log(chalk.greenBright("Botting Google Site!", chalk.yellowBright(count.value += 1)))
-	})
-	t++
-	if (t > 9999999999999) clearInterval(int)
-}, 300)
-}, 5000);
+
+function spamsite(website) {
+	console.log(chalk.greenBright('Initalizing Script!'))
+	var wait = setTimeout(() => {
+		/* the Threads Start
+		 im new to this threads package so if im doing it wrong please tell me :smile: */
+		staticPool.exec(10).then((result) => {
+			var t = 0
+			var int = setInterval(() => {
+				axios.get(website).catch(error => {
+					console.log(chalk.redBright('ERROR OCCURED, SCRIPT STOPPING!!!'))
+					process.exit()
+				})
+				console.log(chalk.greenBright("Botting Google Site!", chalk.yellowBright(count.value += 1)))
+			})
+			t++
+			if (t > 9999999999999) clearInterval(int)
+		}, 300)
+	}, 5000);
+}
+spamsite(`${website}`)
